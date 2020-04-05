b22601a6ba3ed1d695ef00c0e85879df72c31a2b
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-05 12:28:54 +0530

    Update test_Birthday.py

--- test_Birthday.py
+++ test_Birthday.py
@@ -1,11 +1,10 @@
 import pytest
-import Display_Notifications from Birthday_reminder
-import Birthday from Birthday_reminder
+from Birthday_reminder import Display_Notifications 
+
 
 test_strings=[("02-04-2014","In same month"),("01-01-1997","3Months Ago"),("11-08-2000","4Months from now")]
 
 @pytest.mark.parametrize('date,exp',test_strings)
 def test_Birthday(date,exp):
-    bday= Birthday(date)
-    act=Notification()
+    act=Display_Notifications(date)
     assert act==exp
