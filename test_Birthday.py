6fd42836d6db9f305c8904e0e11812ee60513f46
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-05 13:34:00 +0530

    Update test_Birthday.py

--- test_Birthday.py
+++ test_Birthday.py
@@ -1,6 +1,6 @@
 import pytest
-from Birthday_reminder import Display_Notifications 
-
+from Birthday_reminder_test import Display_Notifications 
+#Birthday_reminder_test has the notification message returned as result
 
 test_strings=[("02-04-2014","In same month"),("01-01-1997","3Months Ago"),("11-08-2000","4Months from now")]
 
